73f7232be0680f529250aec993a94095fb1cdc25
Tin_Le <jackletrungtin@gmail.com>
2022-11-22 09:56:42 -0800

    added an event listenr

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+const clickMeButton = document.querySelector('.click-me')
+
+const handleClickme = function ()
+{
+    alert('We are all going to die anyway');
+}
+
+clickMeButton.addEventListener('click', handleClickme)
+
+
+// const ohno = function ()
+// {
+//     alert('fuck');
+// }
+
+// clickMeButton.addEventListener('click', ohno)
