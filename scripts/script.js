6c09b17967778cdfb6335493f1b96ffe1a66f3c3
Tin_Le <jackletrungtin@gmail.com>
2022-11-22 10:39:31 -0800

    added more event listeners

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,38 @@ const clickMeButton = document.querySelector('.click-me')
 
 const handleClickme = function ()
 {
-    alert('What a rebel! Good for you!');
+    alert('China is gone');
     // clickMeButton.removeEventListener('click', handleClickme);
 }
 
 clickMeButton.addEventListener('click', handleClickme, {once:true})
 
+// clickMeButton.addEventListener('click', handleClickme, {once:true})
+
+
+const changeBG = function ()
+{
+    // document.body.style.backgroundColor = "pink"
+    document.body.classList.add("pink");
+}
+
+clickMeButton.addEventListener('click', changeBG)
+
+function changeText()
+{
+    if (clickMeButton.textContent === "Launch ICMB"){
+    clickMeButton.textContent = "Reloading"
+}
+    else if (clickMeButton.textContent ==="Reloading"){
+        clickMeButton.textContent = "Launch ICMB"
+    }
+}
+
+clickMeButton.addEventListener("click", changeText)
 
 // const ohno = function ()
 // {
-//     alert('fuck');
+//     alert('ohnononon');
 // }
 
 // clickMeButton.addEventListener('click', ohno)
